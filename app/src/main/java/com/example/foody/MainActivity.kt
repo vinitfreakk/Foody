dc5dc4cfb1272bfbeba417aaa3563fd779662d82
vinit <vinithanabar20@gmail.com>
2023-07-21 23:38:18 +0530

    added navigation component

---
+++ app/src/main/java/com/example/foody/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.foody
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+
+
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //setting up navigation controller
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.btm_nav)
+        val navController = findNavController(this,R.id.host_fragment)
+
+        NavigationUI.setupWithNavController(bottomNavigation,navController)
+
+    }
+}
